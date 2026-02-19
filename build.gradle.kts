210b41197cc19c4a5b27601482b4d5f98ddde56e
Cyaside <tristan.rasheedsatria@gmail.com>
2026-02-19 23:36:36 +0700

    chore : sonar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,14 @@
 plugins {
     java
-    id("org.springframework.boot") version "4.0.3"
+    id("jacoco")
+    id("org.springframework.boot") version "3.5.10"
     id("io.spring.dependency-management") version "1.1.7"
+    id("org.sonarqube") version "7.1.0.6387"
 }
 
-group = "com.example"
+group = "id.ac.ui.cs.advprog"
 version = "0.0.1-SNAPSHOT"
-description = "Yomu"
+description = "auth"
 
 java {
     toolchain {
@@ -24,16 +26,30 @@ repositories {
     mavenCentral()
 }
 
+sonarqube {
+        properties {
+                property("sonar.projectKey", "advprog-2026-A11-project_be-auth")
+                property("sonar.organization", "adpro-a-kelompok-11")
+        }
+}
+
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     annotationProcessor("org.projectlombok:lombok")
-    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+tasks.withType<JacocoReport> {
+    reports {
+        xml.required.set(true)
+    }
 }
