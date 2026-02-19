90ff43e5729856ace7be003fc57d122c8d3dedbc
raidakhoyyara <raidakhoyyara247@gmail.com>
2026-02-20 00:12:31 +0700

    add ci-cd

--- src/main/java/id/ac/ui/cs/advprog/beachievement/BeAchievementApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/beachievement/BeAchievementApplication.java
@@ -5,10 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BeAchievementApplication {
-
     public static void main(String[] args) {
-
         SpringApplication.run(BeAchievementApplication.class, args);
     }
-
 }
