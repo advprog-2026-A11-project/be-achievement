51eeac4560e16bb96c102a9fe755c40901ad0e1c
raidakhoyyara <raidakhoyyara247@gmail.com>
2026-02-20 19:49:24 +0700

    fix: enable cors for frontend integration

--- src/main/java/id/ac/ui/cs/advprog/beachievement/controller/AchievementController.java
+++ src/main/java/id/ac/ui/cs/advprog/beachievement/controller/AchievementController.java
@@ -5,12 +5,14 @@ import id.ac.ui.cs.advprog.beachievement.service.AchievementService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/achievement")
 public class AchievementController {
