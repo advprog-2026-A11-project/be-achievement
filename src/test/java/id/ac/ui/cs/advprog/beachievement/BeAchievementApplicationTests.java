4982c07c39b8344bdc1a10c03d98cf1016486f86
raidakhoyyara <raidakhoyyara247@gmail.com>
2026-03-06 20:26:44 +0700

    fix test

--- src/test/java/id/ac/ui/cs/advprog/beachievement/BeAchievementApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/beachievement/BeAchievementApplicationTests.java
@@ -8,8 +8,8 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles("test")
 class BeAchievementApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+  @Test
+  void contextLoads() {
+  }
 
 }
