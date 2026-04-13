e39d5446c7243260aa5db001718f42588f02afe2
raidakhoyyara <raidakhoyyara247@gmail.com>
2026-04-13 14:21:09 +0700

    style: fix import order in UserDailyMission for checkstyle

--- src/main/java/id/ac/ui/cs/advprog/beachievement/model/UserDailyMission.java
+++ src/main/java/id/ac/ui/cs/advprog/beachievement/model/UserDailyMission.java
@@ -1,9 +1,9 @@
 package id.ac.ui.cs.advprog.beachievement.model;
 
 import jakarta.persistence.*;
+import java.util.UUID;
 import lombok.Getter;
 import lombok.Setter;
-import java.util.UUID;
 
 @Entity
 @Table(name = "user_daily_mission")
