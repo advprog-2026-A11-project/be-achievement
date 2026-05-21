7b89e274058a63a056d328b026608297d9abc7d3
raidakhoyyara <raidakhoyyara247@gmail.com>
2026-05-21 18:53:43 +0700

    fix: checkstyle

--- src/main/java/id/ac/ui/cs/advprog/beachievement/repository/UserDailyMissionRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/beachievement/repository/UserDailyMissionRepository.java
@@ -1,14 +1,15 @@
 package id.ac.ui.cs.advprog.beachievement.repository;
 
-import id.ac.ui.cs.advprog.beachievement.model.UserDailyMission;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
+import id.ac.ui.cs.advprog.beachievement.model.UserDailyMission;
 
 public interface UserDailyMissionRepository extends JpaRepository<UserDailyMission, Long> {
   List<UserDailyMission> findByUserId(UUID userId);
@@ -27,13 +28,14 @@ public interface UserDailyMissionRepository extends JpaRepository<UserDailyMissi
   void deleteByDailyMissionId(@Param("missionId") Long missionId);
 
   @Modifying
-  @Query(value = """
+  @Query(value = 
+    """
       INSERT INTO user_daily_mission (
           user_id, mission_id, current_progress, is_completed, reward_claimed
       )
       VALUES (:userId, :missionId, 0, FALSE, FALSE)
       ON CONFLICT (user_id, mission_id) DO NOTHING
-      """, nativeQuery = true)
+    """, nativeQuery = true)
   void insertIfMissing(@Param("userId") UUID userId, @Param("missionId") Long missionId);
 
   @Query("SELECT COALESCE(SUM(dm.rewardPoints), 0) FROM UserDailyMission udm "
